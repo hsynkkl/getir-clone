4d01f307422e210895fccd50dcba735dbdf6bb7f
hsyn <kak.hus@hotmail.com>
2023-08-12 18:56:19 +0300

    first commit

---
+++ GetirClone/src/screens/HomeScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import styles from "./styles";
+function index() {
+  return (
+    <View style={styles.headerMain}>
+      <View></View>
+      <View></View>
+    </View>
+  );
+}
+
+export default index;
---
+++ GetirClone/src/screens/HomeScreen/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet, Dimensions } from "react-native";
+const { height } = Dimensions.get("window");
+const styles = StyleSheet.create({
+  headerMain: {
+    height: 30,
+    backgroundColor: "red",
+  },
+});
+export default styles;
